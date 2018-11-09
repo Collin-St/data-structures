770e8fa1c514adc32e5f49acb853aac050904ecf
Collin-St <collin.street.dr@gmail.com>
2018-11-08 22:43:52 -0800

    Provided complete solutions for linkedList.js tree.js

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,16 +2,52 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
-
+  
+  // console.log(list);
   list.addToTail = function(value) {
-  };
+    // takes a value, adds it to end of list
+  var newNode = new Node(value);
+  
+  //for the first invocation of addToTail
+  if (list.head === null){
+    list.head = newNode;
+    list.tail = newNode;
+    // console.log(list)
+  //the rest
+  } else {
+    var previousTail = list.tail;
+    list.tail = newNode;
+    previousTail.next = newNode;
+  }
 
+  };
+  // console.log(list);
   list.removeHead = function() {
+    // removes the first node from the list, and returns
+    // its value
+    var head = list.head.value;
+    list.head = list.tail;
+    return head;
   };
 
   list.contains = function(target) {
-  };
+    // returns boolean reflecting whether or not the 
+    // passed-in value is in the linked list
+    if (list.head === target) {
+      return true;
+    };
 
+    var node = list.head;
+    // console.log(node);
+    while (node) {
+        if (node.value === target) {
+      return true;
+    }
+    node = node.next;
+    // console.log(node);
+  }
+  return false;
+  };
   return list;
 };
 
@@ -26,4 +62,5 @@ var Node = function(value) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
+    - Linear
  */
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,20 +2,43 @@ var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
 
-  // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = []; 
+  extend(newTree, treeMethods);
+  console.log(newTree);
   return newTree;
+};
+
+var extend = function(obj, methods) {
+	for (var key in methods) {
+		obj[key] = methods[key];
+	}
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+	//make a new tree
+	//add the child to tree/child array
+	// console.log(this);
+	// console.log(newTree);
+	this.children.push(new Tree(value));
+
 };
 
 treeMethods.contains = function(target) {
+	
+	var foundChild = false;
+	if (this.value === target) {
+		foundChild = true;
+	} else {
+		for(var i = 0; i < this.children.length; i++) {
+			if(this.children[i].contains(target)) {
+				foundChild = true;
+			}
+		}
+	} 
+	return foundChild;
 };
-
 
 
 /*
